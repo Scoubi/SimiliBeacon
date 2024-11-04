c66e53de5e1dcd3408a0a1500b7cfb7ae5c441d6
Edwin Amsler <EdwinGuy@GMail.com>
2024-11-04 11:09:08 -0600

    Some quick code cleanup:
    * Let rust handle the data types for m, y, and others
    * Use a `for` loop to remove the need to count for ourselves

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,27 @@ use std::{process, thread::sleep, time::Duration};
 
 fn main() {
     
-    let m: i8 = 60; // Default time in minutes to wait between each URL Request
+    let m = 60; // Default time in minutes to wait between each URL Request
     let uri = vec!["CONTRIBUTING.md", "CodeQL.yml", "LICENSE.txt", "README.md", "SECURITY.md", "cglicenses.json", "cgmanifest.json", "gulpfile.js", "package.json", "product.json"];
-    let mut x: i8 = 0; // count the number of itteration to the loop
-    let mut y: i8 = thread_rng().gen_range(1,11); // Number of loop to do before changing the URI
+    let mut y; // Number of loop to do before changing the URI
     let fqdn = "https://github.com/microsoft/vscode/".to_string().to_owned(); // Domain we use for our C2 config
-    let mut url = fqdn.clone() + uri.choose(&mut rand::thread_rng()).unwrap(); // Full URL where to grab the C2 instructions
+    let mut url; // Full URL where to grab the C2 instructions
 
     loop {
-        let jit: f32 = thread_rng().gen_range(0.0,2.0);
-        let ttw8: u64 = (m as f32 * 60.0 * jit) as u64; // Time to wait in minutes
-        let minutes = Duration::from_secs(ttw8.into());
+        y = thread_rng().gen_range(1,11);
+        url = fqdn.clone() + uri.choose(&mut rand::thread_rng()).unwrap();
 
-        println!("Minutes to wait: {} URL to Fetch: {}", ttw8/60, url);
-        sleep(minutes);
-        if reqwest::blocking::get(url.clone()).is_err()  {
-            println!("Request failed. Unable to get {url}");
-            process::exit(1);
-        }
+        for _ in 0 .. y {
+            let jit: f32 = thread_rng().gen_range(0.0,2.0);
+            let ttw8: u64 = (m as f32 * 60.0 * jit) as u64; // Time to wait in minutes
+            let minutes = Duration::from_secs(ttw8.into());
 
-        x+=1;
-
-        if x == y {
-            y = thread_rng().gen_range(1,11);
-            x = 0;
-            url = fqdn.clone() + uri.choose(&mut rand::thread_rng()).unwrap();
+            println!("Minutes to wait: {} URL to Fetch: {}", ttw8/60, url);
+            sleep(minutes);
+            if reqwest::blocking::get(url.clone()).is_err()  {
+                println!("Request failed. Unable to get {url}");
+                process::exit(1);
+            }
         }
     }
 }
